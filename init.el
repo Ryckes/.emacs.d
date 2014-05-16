ad3583f6488132cf4d9d10cfbda008810266feb2
Ryckes <kevin.mnmn@gmail.com>
2014-05-16 10:04:53 +0200

    Refactored personal-config into init.el

--- init.el
+++ init.el
@@ -1,16 +1,30 @@
 
+(setq inhibit-startup-screen t)
+
 ;; No backup files
 (setq make-backup-files nil)
+
+; Remove menus and toolbar
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
 
 (add-to-list 'load-path user-emacs-directory)
 (add-to-list 'load-path (expand-file-name "elisp" user-emacs-directory))
 
+; Get 'package' if not present and define some utilities
 (require 'init-dep)
-
-; After elpa
-(load "personal-config")
-
-(setq inhibit-startup-screen t)
 
 (require 'init-c)
 (require 'init-latex)
+(require 'init-java)
+(require 'init-web)
+(require 'init-git)
+(require 'init-utils)
+(require 'init-snippets)
+; Currently disabled:
+; (require 'init-autocomplete)
+
+; Dark theme
+(when (>= emacs-major-version 24)
+  (load-theme 'deeper-blue))
