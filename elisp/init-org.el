4401c08270c170b354b0970eb9a7f3cca55a6f35
Ryckes <kevin.mnmn@gmail.com>
2014-08-27 19:31:58 +0200

    Changes in org setup

--- elisp/init-org.el
+++ elisp/init-org.el
@@ -37,7 +37,7 @@
       '(("t" "Task" entry (file+headline "~/org/organizer.org" "Inbox")
 	 "* TODO %?\n %i\n")
 	("p" "Project" entry (file "~/org/organizer.org")
-	 "* %?\n %i\n")
+	 "* %? :PROJECT:\n %i\n")
 	("w" "Work" entry (file "~/org/business.org")
 	 "* TODO %?\n %i\n"))
       
@@ -46,7 +46,7 @@
 	 ((org-agenda-list nil nil 1)
 	  (tags "PROJECT/-WAITING-DONE")
 	  (tags-todo "/WAITING")
-	  (tags-todo "-MAYBE-SCHEDULED={.+}/-WAITING")
+	  (tags-todo "-MAYBE-SCHEDULED={.+}-DEADLINE={.+}/-WAITING")
 	  (stuck)))
 	("p" tags "PROJECT-MAYBE-DONE" nil)
 	("m" tags "PROJECT&MAYBE" nil)))
@@ -70,6 +70,20 @@
 	    (define-key org-agenda-mode-map "5" (lambda () (interactive) (org-agenda-todo "DONE")))
 	    (define-key org-agenda-mode-map (kbd "K t") 'ryckes/org-agenda-schedule-today)
 	    (define-key org-agenda-mode-map (kbd "K o") 'ryckes/org-agenda-schedule-tomorrow)))
+
+(defun ryckes/org-add-tag (tag)
+  "Add tag TAG to the list of tags of the current task"
+  (interactive "MNew tag: ")
+  (setq tags (org-get-tags-at))
+  (push tag tags)
+  (org-set-tags-to (delete-dups tags)))
+
+(add-hook 'org-mode-hook
+	  (lambda ()
+	    (setq truncate-lines nil)
+	    (global-unset-key (kbd "C-t"))
+	    (define-key org-mode-map (kbd "C-t h") (lambda () (interactive) (ryckes/org-add-tag "@HOME")))
+	    (define-key org-mode-map (kbd "C-t c") (lambda () (interactive) (ryckes/org-add-tag "@COLLEGE")))))
 
 ;; Thanks to Sacha Chua (sachachua.com)
 (eval-after-load 'org
