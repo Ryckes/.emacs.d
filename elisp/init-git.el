b1cd91b8bdbdfdd8ddb5a9ea5fb6b56de42bf0c7
Ryckes <kevin.mnmn@gmail.com>
2014-09-08 23:36:15 +0200

    Install magit if compatible

--- elisp/init-git.el
+++ elisp/init-git.el
@@ -1,4 +1,6 @@
-;; Magit bindings
-(global-set-key (kbd "C-x g") 'magit-status)
+(unless (string< emacs-version "24")
+  (package-install-if-missing "magit")
+  ;; Magit bindings
+  (global-set-key (kbd "C-x g") 'magit-status))
 
 (provide 'init-git)
