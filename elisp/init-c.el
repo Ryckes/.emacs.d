02de057ac84e7dd72aadeba61fd3d0f59b2511d3
Ryckes <kevin.mnmn@gmail.com>
2014-11-28 18:27:51 +0100

    M-x custom compile commands for C, C++ and Java

--- elisp/init-c.el
+++ elisp/init-c.el
@@ -1,8 +1,46 @@
+(require 'cc-mode)
+
 ;; Good c++ indentation
 (setq c-default-style "linux"
     c-basic-offset 4)
 
-(require 'cc-mode)
+;; Indent macros to the same level of the code
+(add-to-list 'c-offsets-alist '(cpp-macro . 0))
+(add-to-list 'c-offsets-alist '(comment-intro . 0))
+
 (define-key c-mode-base-map (kbd "RET") 'newline-and-indent)
+
+;; From EmacsWiki
+(require 'compile)
+(add-hook 'c++-mode-hook
+	  (lambda ()
+	    (unless (file-exists-p "Makefile")
+	      (set (make-local-variable 'compile-command)
+		   ;; emulate make's .c.o implicit pattern rule, but with
+		   ;; different defaults for the CC, CPPFLAGS, and CFLAGS
+		   ;; variables:
+		   ;; $(CC) -c -o $@ $(CPPFLAGS) $(CFLAGS) $<
+		   (let ((file (file-name-nondirectory buffer-file-name)))
+		     (format "%s -c -o %s.o %s %s %s"
+			     (or (getenv "CC") "g++")
+			     (file-name-sans-extension file)
+			     (or (getenv "CPPFLAGS") "-DDEBUG=9")
+			     (or (getenv "CFLAGS") "-ansi -pedantic -Wall -g")
+			     file))))))
+(add-hook 'c-mode-hook
+	  (lambda ()
+	    (unless (file-exists-p "Makefile")
+	      (set (make-local-variable 'compile-command)
+		   ;; emulate make's .c.o implicit pattern rule, but with
+		   ;; different defaults for the CC, CPPFLAGS, and CFLAGS
+		   ;; variables:
+		   ;; $(CC) -c -o $@ $(CPPFLAGS) $(CFLAGS) $<
+		   (let ((file (file-name-nondirectory buffer-file-name)))
+		     (format "%s -c -o %s.o %s %s %s"
+			     (or (getenv "CC") "gcc")
+			     (file-name-sans-extension file)
+			     (or (getenv "CPPFLAGS") "-DDEBUG=9")
+			     (or (getenv "CFLAGS") "-ansi -pedantic -Wall -g")
+			     file))))))
 
 (provide 'init-c)
--- elisp/init-java.el
+++ elisp/init-java.el
@@ -8,4 +8,14 @@
 ("\\(\\[\\|\\]\\|[|!\\.\\+\\=\\&\\<\\>]\\|-\\|\\/\\|\\%\\|\\*\\|,\\|(\\|)\\|>\\|<\\|{\\|}\\)" 1 font-lock-operator-face )
 ("\\(;\\)" 1 font-lock-end-statement) ))
 
+(require 'compile)
+(add-hook 'java-mode-hook
+	  (lambda ()
+	    (set (make-local-variable 'compile-command)
+		 (let ((file (file-name-nondirectory buffer-file-name)))
+		   (format "javac %s"
+			   file)))))
+(define-key java-mode-map (kbd "C-c C-c") 'compile)
+(define-key java-mode-map (kbd "C-c C-r") 'recompile)
+
 (provide 'init-java)
