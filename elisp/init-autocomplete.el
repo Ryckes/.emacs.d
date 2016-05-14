cda74a59aadc0397efedda1d9039009a0a1ded85
Ryckes <kevin.mnmn@gmail.com>
2016-05-14 10:50:48 +0200

    Add LaTeX autocompletion

--- elisp/init-autocomplete.el
+++ elisp/init-autocomplete.el
@@ -4,8 +4,18 @@
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/auto-complete/ac-dict")
 (ac-config-default)
+
 ;; Compatibiliy of YASnippet and auto-complete
 (setq ac-source-yasnippet nil)
+
+(require 'ac-math)
+(add-to-list 'ac-modes 'latex-mode)
+(add-hook 'LaTeX-mode-hook
+          (lambda ()
+            (setq ac-sources
+                  (append '(ac-source-latex-commands)
+                          ac-sources))))
+
 (define-key ac-complete-mode-map (kbd "RET") nil)
 (define-key ac-menu-map (kbd "<tab>") 'ac-next)
 (setq
