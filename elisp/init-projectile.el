16a2c73e7b60141c602f7f42389417c6c7c15f25
Ryckes <kevin.mnmn@gmail.com>
2016-05-14 10:51:17 +0200

    Do not track node dependency files in projectile

--- elisp/init-projectile.el
+++ elisp/init-projectile.el
@@ -4,6 +4,8 @@
 
   (projectile-global-mode)
   (setq projectile-enable-caching t)
+  (add-to-list 'projectile-globally-ignored-directories
+               "node_modules")
 
   ;; No keybindings, using Helm alternatives
   (global-set-key (kbd "C-x p") 'projectile-find-file)
