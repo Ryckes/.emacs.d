12512e20f35276b3a84a90222227c7e3517b134d
Ryckes <kevin.mnmn@gmail.com>
2016-05-14 10:51:43 +0200

    Correct Octave feature check

--- elisp/init-octave.el
+++ elisp/init-octave.el
@@ -4,17 +4,18 @@
   (require 'octave))
 
 (if (featurep 'octave)
-    (add-to-list 'auto-mode-alist '("\\.m" . octave-mode))
+    (progn
+      (add-to-list 'auto-mode-alist '("\\.m" . octave-mode))
 
-  (add-hook 'octave-mode-hook
-            (lambda ()
-              (auto-complete-mode)
-              (setq octave-block-offset 4)
-              (set (make-local-variable 'compile-command)
-                   (let ((file (file-name-nondirectory buffer-file-name)))
-                     (format "octave -q %s"
-                             file)))))
-  (define-key octave-mode-map (kbd "C-c C-c") 'compile)
-  (define-key octave-mode-map (kbd "C-c C-r") 'recompile))
+      (add-hook 'octave-mode-hook
+                (lambda ()
+                  (auto-complete-mode)
+                  (setq octave-block-offset 4)
+                  (set (make-local-variable 'compile-command)
+                       (let ((file (file-name-nondirectory buffer-file-name)))
+                         (format "octave -q %s"
+                                 file)))))
+      (define-key octave-mode-map (kbd "C-c C-c") 'compile)
+      (define-key octave-mode-map (kbd "C-c C-r") 'recompile)))
 
 (provide 'init-octave)
