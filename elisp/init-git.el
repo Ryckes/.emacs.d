178e16f88067764f6ead56e2e76bc876881aba48
Ryckes <kevin.mnmn@gmail.com>
2016-05-14 10:54:42 +0200

    Fix magit script for Emacs 24.3

--- elisp/init-git.el
+++ elisp/init-git.el
@@ -1,4 +1,6 @@
-(unless (string< emacs-version "24")
+(unless (or (string< emacs-version "24")
+            (and (string-equal emacs-version "24")
+                 (string< emacs-minor-version "4")))
   (package-install-if-missing "magit")
   (setq magit-last-seen-setup-instructions "1.4.0")
   
